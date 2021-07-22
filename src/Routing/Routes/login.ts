9505115785e71b5d32492c412b0cd6df5a577034
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2021-07-22 20:52:52 +0530

    Underwork

---
+++ src/Routing/Routes/login.ts
@@ -0,0 +1,15 @@
+import {Router, Request , Response} from 'express';
+const UserRouter = Router();
+
+
+
+
+UserRouter.get('/', (res:Response,req:Request) => {
+
+
+
+})
+
+
+
+export default UserRouter;
